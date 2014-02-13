38a82a77995d0aea18d850bc76bc53f3c8880b1f
Philipp Hansch <dev@phansch.net>
2014-02-13 16:36:51 +0100

    Add method that returns todays events

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -4,4 +4,12 @@ class Calendar < ActiveRecord::Base
   validates :token_read, :token_write, presence: true, on: :create
   validates :token_read, :token_write, uniqueness: true, on: :create
   has_many :events
+
+  def today
+    day_start = DateTime.now.beginning_of_day
+    day_end = DateTime.now.end_of_day
+
+    events.where('ends_at > ?', day_start).
+      where('starts_at < ?', day_end)
+  end
 end
--- spec/models/calendar_spec.rb
+++ spec/models/calendar_spec.rb
@@ -19,21 +19,31 @@ describe Calendar do
   it { should callback(:generate_write_token).before(:validation) }
   it { should have_many(:events) }
 
+  it "sets the read and write tokens" do
+    @calendar = create(:calendar)
+    expect(@calendar.token_read).not_to be_nil
+    expect(@calendar.token_write).not_to be_nil
+  end
+
   it "should return a list of events for this month"
   it "should return a list of events for this week"
+
   it "should return a list of events for today" do
     @event_yesterday = create(:event,
                               starts_at: 2.days.ago,
-                              ends_at: 1.day.ago)
+                              ends_at: 1.day.ago,
+                              calendar: @calendar)
     @event_today = create(:event,
                           starts_at: DateTime.now,
-                          ends_at: DateTime.now + 5.minutes)
+                          ends_at: DateTime.now + 5.minutes,
+                         calendar: @calendar)
     @event_tomorrow = create(:event,
                              starts_at: DateTime.tomorrow,
-                             ends_at: DateTime.tomorrow + 5.minutes)
-    expect(@calendar.events).to include(@event_today)
-    expect(@calendar.events).not_to include(@event_tomorrow)
-    expect(@calendar.events).not_to include(@event_yesterday)
+                             ends_at: DateTime.tomorrow + 5.minutes,
+                            calendar: @calendar)
+    expect(@calendar.today).to include(@event_today)
+    expect(@calendar.today).not_to include(@event_tomorrow)
+    expect(@calendar.today).not_to include(@event_yesterday)
 
   end
 end
