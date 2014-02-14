84d1c5bdcbde54527d1ea5e933e9e3bb31642da3
Philipp Hansch <dev@phansch.net>
2014-02-14 16:30:32 +0100

    Ensure that tokens are unique for both types

--- app/models/concerns/tokenable.rb
+++ app/models/concerns/tokenable.rb
@@ -1,4 +1,4 @@
-# From http://stackoverflow.com/a/12109098
+# From http://stackoverflow.com/a/12109098 & modified
 module Tokenable
   extend ActiveSupport::Concern
 
@@ -10,15 +10,21 @@ module Tokenable
 
   def generate_read_token
     self.token_read = loop do
-      random_token = SecureRandom.urlsafe_base64(nil, false)
-      break random_token unless self.class.exists?(token_read: random_token)
+      break random_token unless token_exists?(random_token)
     end
   end
 
   def generate_write_token
     self.token_write = loop do
-      random_token = SecureRandom.urlsafe_base64(nil, false)
-      break random_token unless self.class.exists?(token_write: random_token)
+      break random_token unless token_exists?(random_token)
     end
+  end
+
+  def token_exists?(token)
+    self.class.exists?(token_read: token) && self.class.exists?(token_write: token)
+  end
+
+  def random_token
+    SecureRandom.urlsafe_base64(nil, false)
   end
 end
