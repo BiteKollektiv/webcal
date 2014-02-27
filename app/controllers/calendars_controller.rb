9fdf41604f5340e83361b166750a9590f5c5efc5
Philipp Hansch <dev@phansch.net>
2014-02-27 16:30:38 +0100

    Delete file after uploading

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -24,6 +24,7 @@ class CalendarsController < ApplicationController
     allowed = ["text/calendar"]
     if file = Uploader.upload(params[:ical], allowed)
       @calendar.import(file)
+      File.delete(file)
       redirect_to @calendar, notice: t('calendar.import.success')
     else
       redirect_to @calendar, notice: t('calendar.import.invalidfiletype')
