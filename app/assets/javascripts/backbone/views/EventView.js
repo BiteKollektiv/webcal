a6607722d9bc03316b9a59cff98a58a21b4df855
Giorgia Sambrotta <g.sambrotta@gmail.com>
2014-03-07 11:05:19 +0100

    Display fullCalendar

---
+++ app/assets/javascripts/backbone/views/EventView.js
@@ -0,0 +1,27 @@
+var EventsView = Backbone.View.extend({
+	collection: events,
+
+    initialize: function(){
+        _.bindAll(this, 'addAll');
+
+        this.collection.bind('reset', this.addAll);
+    },
+
+	render: function(){
+		this.$el.fullCalendar({
+			header: {
+                    left: 'prev,next today',
+                    center: 'title',
+                    right: 'month,basicWeek,basicDay',
+                    ignoreTimezone: false
+                },
+                selectable: true,
+                selectHelper: true,
+                editable: true
+        });
+	},
+
+	addAll: function(){
+		this.$el.fullCalendar ("addEventSource", this.collection.toJSON());
+	}
+});
