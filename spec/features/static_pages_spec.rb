46d601f8f0a42c7ee5c7b9ec35efc39f1dd265b5
Philipp Hansch <dev@phansch.net>
2014-03-07 16:55:23 +0100

    Disable spec

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe 'calendar user' do
   it 'should create a new calendar from the homepage' do
+    pending
     visit root_path
     click_button('Create calendar')
     expect(page).to have_content('Calendar was successfully created')
